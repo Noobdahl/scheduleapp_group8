2c49a2dcf5ff357b1b1d51752c283753c63ed4c4
Noobdahl <michael.nordahl@hotmail.com>
2023-01-30 10:36:58 +0100

    Test deletecommit

--- specs/step_definitions/mickefeature.js
+++ specs/step_definitions/mickefeature.js
@@ -1,6 +1,6 @@
 import { Given, When, And, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given("att jag är inloggad med {string} and {string}", (email, password) => {
+Given("m att jag är inloggad med {string} and {string}", (email, password) => {
     cy.visit("/");
     cy.get('#email').type(email);
     cy.get('#password').type(password);
@@ -8,16 +8,20 @@ Given("att jag är inloggad med {string} and {string}", (email, password) => {
 
 });
 
-And("jag är i kontrollpanelen", () => {
+And("m jag är i kontrollpanelen", () => {
     cy.get('[onclick=\"location.href=\'/admin\'"]').click();
 })
 
-Then("bilden borde synas", () => {
+Then("m bilden borde synas", () => {
     
     cy.get('img').should("be.visible");
 })
 
-Then("är jag i kontrollpanelen", () => {
+Then("m är jag i kontrollpanelen", () => {
     cy.get('.MuiCardHeader-content > .MuiTypography-root').should("contain", "Welcome");
     
+})
+
+Given("m att jag är inloggad", () => {
+    cy.get('img').should("be.visible");
 })
