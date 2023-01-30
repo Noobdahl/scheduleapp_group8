d58ca221bd24d5390a27f9d19d5fe3f02d4e8ea8
Minderg <105292841+Minderg@users.noreply.github.com>
2023-01-30 10:37:24 +0100

    update.... gabriel

--- specs/gabrielfeature.feature
+++ specs/gabrielfeature.feature
@@ -1,24 +1,24 @@
 Feature: Scheduleapp
 
     Scenario: Logga in
-    Given att jag är inloggad med "exempel@school.net" and "abc123"
-    Then så ser jag schemat
+    Given g att jag är inloggad med "exempel@school.net" and "abc123"
+    Then g så ser jag schemat
 
     Scenario: Komma till dagens datum
-    Given att jag är inloggad med "exempel@school.net" and "abc123"
-    And klickar på till idag
-    Then så kommer jag till dagens datum
+    Given g att jag är inloggad med "exempel@school.net" and "abc123"
+    And g klickar på till idag
+    Then g så kommer jag till dagens datum
 
     Scenario: Komma till inställningar
-    Given att jag är inloggad med "exempel@school.net" and "abc123"
-    And klickar på admin knappen
-    Then kommer till admin sida
+    Given g att jag är inloggad med "exempel@school.net" and "abc123"
+    And g klickar på admin knappen
+    Then g kommer till admin sida
 
 
     Scenario: Nästa sida
-    Given att jag är inloggad med "exempel@school.net" and "abc123"
-    When jag klickar på admin
-    Then så är jag på admin sida
-    Then klickar på teachers
+    Given g att jag är inloggad med "exempel@school.net" and "abc123"
+    When g jag klickar på admin
+    Then g så är jag på admin sida
+    Then g klickar på teachers
     
 
--- specs/step_definitions/gabrielfeature.js
+++ specs/step_definitions/gabrielfeature.js
@@ -2,7 +2,7 @@
 import { Given, When, And, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 // Logga in sidan
-Given("att jag är inloggad med {string} and {string}", (email, password) => {
+Given("g att jag är inloggad med {string} and {string}", (email, password) => {
   cy.visit("/");
   cy.get('#email').type(email);
   cy.get("#password").type(password);
@@ -10,35 +10,35 @@ Given("att jag är inloggad med {string} and {string}", (email, password) => {
    
 });
 
-Then("så ser jag schemat", () => {
+Then("g så ser jag schemat", () => {
   cy.get('img').should('be.visible');
 });
 
-And("klickar på till idag", () => {
+And("g klickar på till idag", () => {
   cy.get('#goToToday').click();
 })
 
-And("klickar på admin knappen", () =>{
+And("g klickar på admin knappen", () =>{
   cy.get('[onclick="location.href=\'/admin\'"]').click();
 })
 
-Then("kommer till admin sida", () =>{
+Then("g kommer till admin sida", () =>{
   cy.get('.MuiCardHeader-content > .MuiTypography-root').should("contain", "Welcome");
 })
 
-Then("så kommer jag till dagens datum", () => {
+Then("g så kommer jag till dagens datum", () => {
   cy.get('aside > table > tbody > .today > :nth-child(1)').should('contain', "2023");
 })
 
-When("jag klickar på admin", () =>{
+When("g jag klickar på admin", () =>{
   cy.get('[onclick="location.href=\'/admin\'"]').click();
 })
 
-Then("så är jag på admin sida", () => {
+Then("g så är jag på admin sida", () => {
   cy.get('.MuiCardHeader-content > .MuiTypography-root').should("contain", "Welcome");
 })
 
-Then("klickar på teachers", () =>{
+Then("g klickar på teachers", () =>{
   cy.get('.RaMenuItemLink-active').click();
 })
 
