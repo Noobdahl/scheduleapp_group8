667e72944ccd381ff57d38ca7bb1077104d10a42
Koffepomp <Zephs1986@gmail.com>
2023-02-05 00:41:09 +0100

    Koffefeatures polerad och fin

--- specs/koffefeature.feature
+++ specs/koffefeature.feature
@@ -2,15 +2,15 @@ Feature: How to use the Scheduleapp
 
     #Login
     Scenario: Login to the scheduleapp
-    Given k That i've entered proper credentials such as "exempel@school.net" and "abc123"
-    And k I am not logged in
-    Then k I can now see the schedule and classes
+    Given k That I am on the homepage
+    And k That i've entered proper credentials such as "exempel@school.net" and "abc123"
+    When k I click login button
+    Then k I am now logged in
 
     #Today
     Scenario: View current date
     Given k That I am logged in
     And k I am on the schedule page
-    And k The current date is not visible on the screen
     When k I press the Today button
     Then k Page will scroll up or down until it reaches todays date
 
@@ -19,4 +19,4 @@ Feature: How to use the Scheduleapp
     Given k That I am logged in
     And k I am on the schedule page
     When k I click on the logout button
-    Then k I am sent to the login page and logged out
+    Then k I am sent to the login page
--- specs/step_definitions/koffefeature.js
+++ specs/step_definitions/koffefeature.js
@@ -1,28 +1,50 @@
 import { Given, When, And, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 // Scenario: Login
-When("k I click the login button after entering the information {string} and {string}", (email, password) => {
+Given("k That I am on the homepage", () => {
   cy.visit("/");
   cy.url().should('contain', "login");
+});
+
+And("k That i've entered proper credentials such as {string} and {string}", (email, password) => {
   cy.get('#email').type(email);
   cy.get("#password").type(password);
+  cy.get('#email').should('have.value', email);
+  cy.get('#password').should('have.value', password);
+});
+
+When("k I click login button", () => {
   cy.get('[type="submit"]').click(); 
 });
 
-// Scenario: View todays date
-When("k I click the Today button", () => {
+Then ("k I am now logged in", () => {
+  cy.url().should('eq', "http://localhost:7655/");
+});
+
+Given("k That I am logged in", () => {
+  cy.visit("/");
+  cy.get('#email').type("exempel@school.net");
+  cy.get("#password").type("abc123");
+  cy.get('[type="submit"]').click(); 
+  cy.url().should('eq', "http://localhost:7655/");
+});
+
+And("k I am on the schedule page", () => {
+  cy.url().should('eq', "http://localhost:7655/");
+});
+
+When("k I press the Today button", () => {
   cy.get('#goToToday').click();
 });
 
-// Scenario: At todays date
-Then("k I am currently at todays date", () => {
-  cy.get('aside > table > tbody > .today > :nth-child(1)').should('contain', TodaysDate);
+Then("k Page will scroll up or down until it reaches todays date", () => {
+  cy.get('aside > table > tbody > .today > :nth-child(1)').should('contain', (new Date().toISOString()).substring(0, 10));
 });
 
-
-// Scenario: Logout
-Given("k I have pressed the logout button and been sent to the login page", () => {
-  cy.visit("/");
-  cy.url().should('contain', 'login');
+When("k I click on the logout button", () => {
+  cy.get('[onclick="location.href=\'/logout.html\'"]').click();
 });
 
+Then("k I am sent to the login page", () => {
+  cy.url().should('contain', "login");
+});
